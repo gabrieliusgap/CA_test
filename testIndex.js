ba30997f630db6a604fd95aadbb461bb1912dba2
Gabrielius Gaponovas <gabrielius.gaponovas@codeacademylt.onmicrosoft.com>
2021-03-12 13:42:59 +0200

    changed text

--- testIndex.js
+++ testIndex.js
@@ -4,3 +4,6 @@ let span1 = document.getElementsByClassName("span")[0]
 span1.innerText ="blablabla"
 let h1 =document.getElementsByClassName("h1")[0]
 h1.innerText = "jojojo"
+
+span1.innerText ="bliubliubliu"
+h1.innerText = "joujoujou"
