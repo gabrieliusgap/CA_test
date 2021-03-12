642aa5c9174c14905b9c0e0175b9df0c18831089
Gabrielius Gaponovas <gabrielius.gaponovas@codeacademylt.onmicrosoft.com>
2021-03-12 13:41:22 +0200

    added h1 and span, changed style

--- testIndex.js
+++ testIndex.js
@@ -1,2 +1,6 @@
 let main= document.getElementById("main")
 main.innerText = "kjfdsdghjhgddfghhgfddsfh"
+let span1 = document.getElementsByClassName("span")[0]
+span1.innerText ="blablabla"
+let h1 =document.getElementsByClassName("h1")[0]
+h1.innerText = "jojojo"
